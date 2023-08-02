b00f57a9cce8a17c8d3e7f9ce678e3a6f43b87b5
vijaydev133 <97230766+vijaydev133@users.noreply.github.com>
2023-08-02 21:19:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let word = confirm("Do you want to proceed?" );
+// console.log(word);
+
+if(word==true){
+	alert("You clicked OK. Proceeding...");
+	
+}else{
+	alert("You clicked Cancel. Exiting...");
+}
